54ff76c1666703cbae2d54957d0656505aa4d9bf
gcoutini <61057977+gcoutini@users.noreply.github.com>
2021-01-28 22:20:55 -0300

    layout restructuring

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,9 +5,9 @@ import profile from "../../pp.jpg"
 
 class Header extends Component {
   
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return(
--- src/components/layout/Panel.jsx
+++ src/components/layout/Panel.jsx
@@ -5,15 +5,15 @@ import { Home, People, BarChart, Delete } from '@material-ui/icons'
 import logo from "../../logo2.png"
 class Panel extends Component {
  
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return(
       <div className="Panel">
-        <div className='logo'>
-          <img src={logo} alt="Logo"></img>
+        <div className="logo">
+          <img src={logo} alt="Logo" width="125px" height="auto"></img>
         </div>
         <nav className="links">
           <IconButton onClick={() => window.location.href = "/main"}><Home></Home>Home</IconButton>
